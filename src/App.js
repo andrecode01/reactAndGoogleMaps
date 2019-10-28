68e9a2bad9dd1d8d410187930140e8ab8f2a341f
andrecode01 <andrecorradi00@gmail.com>
2019-10-28 00:56:45 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import "./App.css";
+import { Marker ,withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps";
+
+function Map () {
+  return(
+    < GoogleMap
+      defaultZoom = {15}
+      defaultCenter = {{ lat : -20.606356, lng : -41.201441}} 
+    >
+
+      < Marker 
+        onClick = {function funcao() {
+          funcao2(
+            this.getPosition()
+          );
+        }}
+        position = {{ lat : -20.606356, lng : -41.201441}}
+      />
+
+    </ GoogleMap>
+  );
+}
+
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+export default function App() {
+  return (
+    <div style={{margin: '0 auto', width:'80vw', height:'100vh'}}>
+      <h1 class="tittle">React and Google Maps</h1>
+      <WrappedMap 
+        googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places'}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `80vh` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    </div>
+  );
+}
+
+function funcao2(p) {
+  alert("Posição Lati, Long: "+p);
+}
